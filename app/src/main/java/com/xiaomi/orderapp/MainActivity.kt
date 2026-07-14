358823f995d2a66c365516471120453f27256dcd
pasupathiveerappan21-creator <pasupathiveerappan21@gmail.com>
2026-07-15 01:56:08 +0530

    Create MainActivity.kt

---
+++ app/src/main/java/com/xiaomi/orderapp/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.xiaomi.orderapp
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<android.widget.Button>(R.id.btnDealers).setOnClickListener {
+            startActivity(Intent(this, DealersActivity::class.java))
+        }
+        findViewById<android.widget.Button>(R.id.btnProducts).setOnClickListener {
+            startActivity(Intent(this, ProductsActivity::class.java))
+        }
+        findViewById<android.widget.Button>(R.id.btnNewOrder).setOnClickListener {
+            startActivity(Intent(this, OrderActivity::class.java))
+        }
+    }
+}
